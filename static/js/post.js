0e6296022158ea7e7ae0400ae9d8b340b3ffd549
jekhy <tansuanna@gmail.com>
2017-09-26 09:38:51 +0800

    fixed js path

--- static/js/post.js
+++ static/js/post.js
@@ -1,13 +1,6 @@
 $(function() {
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    window.disqus_shortname = 'jekhy'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
-
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
         if(href){
@@ -21,11 +14,8 @@ $(function() {
         }
     });
 
-    $.getScript('/js/prettify/prettify.js',function(){
+    $.getScript('/static/js/prettify/prettify.js',function(){
         prettyPrint();
     });
 
-    if(/\#comment/.test(location.hash)){
-        $('#disqus_container .comment').trigger('click');
-    }
 })
